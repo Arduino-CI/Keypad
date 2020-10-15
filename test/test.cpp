ab81917f04f9bf2345973a805ab6dfe0e18c8bbe
James Foster <github@jgfoster.net>
2020-10-14 22:52:50 -0700

    Test passes.

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,12 @@
+/*
+bundle config --local path vendor/bundle
+bundle install
+bundle exec arduino_ci_remote.rb  --skip-compilation
+ */
 #include "Arduino.h"
 #include "ArduinoUnitTests.h"
 #include "Keypad.h"
 
-unittest(test) {
-  assertTrue(true);
-}
+unittest(test) { assertTrue(true); }
 
 unittest_main()
